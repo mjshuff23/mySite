50e176143b7b20440aae0c2bf3d19822e631c78b
Michael Shuff <mjshuff23@gmail.com>
2020-09-09 12:47:27 -0400

    just started

--- public/events.js
+++ public/events.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    fetch("/kitten/image")
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Something went wrong");
+            }
+            return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            console.log(data.src);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 });
